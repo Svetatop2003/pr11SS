2e8cf6c7fb4c599e6b493e0384a6be0598d45d84
S <323@323-11>
2023-10-10 15:18:11 +0300

    first commit

---
+++ pr11SS/BuilderBurger/IBurgerBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using pr11SS.BuilderBurger;
+using pr11SS.DBCon;
+
+namespace pr11SS.BuilderBurger
+{
+    public interface IBurgerBuilder
+    {
+        IBurgerBuilder AddCheese();
+        IBurgerBuilder AddBacon();
+        IBurgerBuilder AddLetuce();
+        IBurgerBuilder AddTomato();
+        IBurgerBuilder AddPickles();
+
+        Burgers GetBurgers();
+    }
+}
